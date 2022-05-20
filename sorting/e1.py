02df16e6f69e137d93db733643cba130ad3c4357
RosFreak <102175551+RosFreak@users.noreply.github.com>
2022-05-20 23:04:43 +0530

    ..

--- sorting/e1.py
+++ sorting/e1.py
@@ -17,38 +17,3 @@ class Solution:
 
 
 
-
-
-# } Driver Code Ends
-
-#{ 
-#  Driver Code Starts
-#Initial Template for Python 3
-
-def main():
-
-    T = int(input())
-
-    while(T > 0):
-    	sz = [int(x) for x in input().strip().split()]
-    	n, k = sz[0], sz[1]
-    	a = [int(x) for x in input().strip().split()]
-    	b = [int(x) for x in input().strip().split()]
-    	ob=Solution()
-    	if ob.isPossible(a, b, n, k):
-    		print(1)
-    	else:
-    		print(0)
-    	
-    	T -= 1
-
-if __name__ == "__main__":
-    main()
-
-
-
-
-
-
-
-# } Driver Code Ends
