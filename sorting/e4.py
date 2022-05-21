2a53ba71ce215d769ef195f36534bb73884109ad
RosFreak <102175551+RosFreak@users.noreply.github.com>
2022-05-21 12:19:38 +0530

    ..

---
+++ sorting/e4.py
@@ -0,0 +1,20 @@
+#User function Template for python3
+
+class Solution:
+    def isPossible(self,a, b, n, k):
+        a.sort()
+        b.sort(reverse = True)
+        c=0
+        for i in range(n):
+            if a[i] + b[i] >= k:
+                continue
+            c=1
+            break
+        if(c==1):
+            return False
+        return True
+
+
+
+
+#same as 1
